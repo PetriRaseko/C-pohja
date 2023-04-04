c43c694ff67f28c1922ec5205c86d57e53d3cf3e
PetriRaseko <127213891+PetriRaseko@users.noreply.github.com>
2023-04-04 09:27:22 +0300

    Update HelloWorldTests.cs

--- C-pohja.Tests/HelloWorldTests.cs
+++ C-pohja.Tests/HelloWorldTests.cs
@@ -1,1 +1,20 @@
+using NUnit.Framework;
 
+namespace C_pohja.Tests
+{
+    public class HelloWorldTests
+    {
+        [Test]
+        public void TestHelloWorld()
+        {
+            // Arrange
+            var expected = "Hello, World!";
+
+            // Act
+            var actual = Program.GetHelloWorld();
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
